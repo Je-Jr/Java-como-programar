fdc8df6493b036b0fb99e9756958badc5d4988db
Jorge Junior <jr.jorge500@yahoo.com>
2023-05-25 16:11:49 -0300

    resolucao limite de creditos

--- capitulo4/Questoes/src/QuilometragemDeCombustivel.java
+++ capitulo4/Questoes/src/QuilometragemDeCombustivel.java
@@ -1,3 +1,14 @@
+/*
+ (Quilometragem de combustível) Os motoristas se preocupam com a quilometragem obtida por seus automóveis. 
+ Um motorista monitorou várias viagens registrando a quilometragem dirigida e a quantidade de combustível 
+ em litros utilizados para cada tanque cheio. Desenvolva um aplicativo Java que receba como entrada os 
+ quilômetros dirigidos e os litros de gasolina consumidos (ambos como inteiros) para cada viagem. O programa 
+ deve calcular e exibir o consumo em quilômetros/litro para cada viagem e imprimir a quilometragem total e a 
+ soma total de litros de combustível consumidos até esse ponto para todas as viagens. Todos os cálculos de 
+ média devem produzir resultados de ponto flutuante. Utilize classe Scanner e repetição controlada por 
+ sentinela para obter os dados do usuário.
+ */
+
 public class QuilometragemDeCombustivel {
     public static void main(String[] args) {
         java.util.Scanner input = new java.util.Scanner(System.in);
