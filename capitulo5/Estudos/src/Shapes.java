ba8b38e220069bf0127eab9f67329b3fdd144c7c
Jorge Junior <jr.jorge500@yahoo.com>
2023-06-01 13:57:32 -0300

    commitando

---
+++ capitulo5/Estudos/src/Shapes.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Shapes extends JPanel {
+  private int choice;
+
+  public Shapes (int userChoice) {
+    choice = userChoice;
+  }
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    for (int i = 0; i < 10; i++) {
+      switch (choice) {
+        case 1:
+          g.dr
+      }
+    }
+  }
+}
