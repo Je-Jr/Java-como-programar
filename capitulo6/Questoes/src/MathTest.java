2f5fdfdd05559758bcce79e3753460540dba9ade
Jorge Junior <jr.jorge500@yahoo.com>
2023-09-22 16:05:18 -0300

    migrando intellij

---
+++ capitulo6/Questoes/src/MathTest.java
@@ -0,0 +1,13 @@
+public class MathTest {
+    public static void main(String[] args) {
+        int x = 27;
+        System.out.println(Math.abs(x));
+        System.out.println(Math.floor(x));
+        System.out.println(Math.abs(x));
+        System.out.println(Math.ceil(x));
+        System.out.println(Math.abs(x));
+        System.out.println(Math.ceil(x));
+        System.out.println(Math.ceil(-Math.abs(-8 + Math.floor(-5.5))));
+
+    }
+}
